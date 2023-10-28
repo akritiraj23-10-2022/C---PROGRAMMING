ab9a318eeb541352ff7a1a0e71d1744c3117ca77
Akriti Raj <121313412+akritiraj23-10-2022@users.noreply.github.com>
2023-10-28 15:56:34 +0530

    Add files via upload
    it is a projects in a C++ programming in  languages in a programming it programming to the   learning to the languages in a programming.

---
+++ IF5.cpp
@@ -0,0 +1,18 @@
+/*write the program even odd numbers */
+#include<iostream>
+using namespace std;
+int main()
+{
+    int no;
+    cout<<"Enter the any numbers\n";
+    cin>>no;
+    if(no%2==0)
+    {
+        cout<<"EVEN";
+    }
+    else
+    {
+        cout<<"odd";
+    }
+    return 0;
+}
